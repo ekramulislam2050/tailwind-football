f3b9a6940a8dcf468210c235548dc5c61480d12d
ekramulislam2050 <ekramulislam2050@gmail.com>
2024-08-18 20:02:53 +0600

    my first tailwind css project

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+   
+    extend: {
+      fontFamily:{
+        'poppins':["Poppins",' sans-serif']
+      },
+      colors:{
+          "btn-bg":['rgba(224,44,109)'],
+          "card-bg":["rgba(255,255,255)"]
+      },
+      background:{
+        'icon-bg':['rgb(150, 155, 171)'],
+      }
+    },
+  },
+  plugins: [],
+}
